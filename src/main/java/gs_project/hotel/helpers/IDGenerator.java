1521fd7d42e80f777d47bfa72497a755a5004402
Writwick <writzx@gmail.com>
2017-07-13 20:17:02 +0530

    added IDGenerator

---
+++ src/main/java/gs_project/hotel/helpers/IDGenerator.java
@@ -0,0 +1,18 @@
+package gs_project.hotel.helpers;
+
+import java.util.Random;
+
+public final class IDGenerator {
+    private static Random random = new Random();
+    private IDGenerator() { }
+
+    public static String generate() {
+        char[] chars = "43VRHQZLYTK17WU2506GOFECDIJ8MXBNAPS9".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 16; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
